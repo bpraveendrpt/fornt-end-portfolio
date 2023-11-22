ea320e1a677da607dcafbd82b499b3279acb5a6c
Praveen kumar Bandi <bpraveendrpt@gmail.com>
2023-11-22 02:02:38 -0500

    added resume

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Resume from './components/Resume';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <Router>
+     
+      <div className="App">
+       <h1>Welcome</h1>
+       
+        <Routes>
+          <Route path="/"  />
+          {/* <Route path="/skillset" element={<Skill />} />
+          <Route path="/project" element={<Project />} /> */}
+          <Route path="/resume" element={<Resume />} />
+          {/* <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/"/>} /> */}
+        </Routes>
+       
+      </div>
+    </Router>
+    
+    
   );
 }
 
